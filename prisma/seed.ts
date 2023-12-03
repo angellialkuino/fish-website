2ba5cb9f3758137b3e9cdc74eb2f9458179ca321
iyabc <juliennepanes@gmail.com>
2023-12-03 20:16:48 +0800

    fixed prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: "admin@admin.com" },
+    update: {},
+    create: {
+      name: "Admin",
+      email: "admin@admin.com",
+      role: "admin",
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit();
+  });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,21 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function Home() {
-  const users = await prisma.user.findMany({
-    select: {
-      name: true,
-      posts: {
-        select: {
-          title: true,
-        },
-      },
-    },
-  });
+  let users = await prisma.user.findMany();
 
   return (
     <>
+      Users:
       {users.map((user) => {
-        <p className="text-red-500">{user.name} dasda</p>;
+        return <>{user.name}</>;
       })}
     </>
   );
